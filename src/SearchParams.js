4133e10570812d14b62d65d7b97289b05a21f4c8
Sebastian Meza <sbstnmz@gmail.com>
2020-04-22 20:36:31 -0400

    add select from breeds list

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -4,6 +4,8 @@ import { ANIMALS } from '@frontendmasters/pet';
 const SearchParams = () => {
   const [location, setLocation] = useState('Seattle, WA');
   const [animal, setAnimal] = useState('Dog');
+  const [breed, setBreed] = useState('');
+  const [breeds, setBreeds] = useState([]);
 
   return (
     <div className="search-params">
@@ -31,6 +33,24 @@ const SearchParams = () => {
             {ANIMALS.map(animal => (
               <option key={animal} value={animal}>
                 {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            name="breed"
+            id="breed"
+            value={breed}
+            onChange={e => setBreed(e.target.value)}
+            onBlur={e => setBreed(e.target.value)}
+            disabled={breeds.length === 0}
+          >
+            <option>All</option>
+            {breeds.map(breedStr => (
+              <option key={breedStr} value={breedStr}>
+                {breedStr}
               </option>
             ))}
           </select>
