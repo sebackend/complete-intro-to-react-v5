d8471b0da4391071b8c8597fc4edb0ee33f92ea1
Sebastian Meza <sbstnmz@gmail.com>
2020-05-01 01:40:48 -0400

    Details component as class based component

--- src/Details.js
+++ src/Details.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import pet from '@frontendmasters/pet';
 
-const Details = props => {
-  return <h1>hi lol {props.id}</h1>;
-};
+class Details extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true
+    };
+  }
+
+  componentDidMount() {
+    pet.animal(this.props.id).then(({ animal }) => {
+      this.setState({
+        name: animal.name,
+        animal: animal.type,
+        location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
+        description: animal.description,
+        media: animal.photos,
+        breed: animal.breeds.primary,
+        loading: false
+      });
+    });
+  }
+
+  render() {}
+}
 
 export default Details;
